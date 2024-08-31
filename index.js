bd92e020c77bcbfa738689d66ffef54dca5cc061
Tomas <tomimacia@gmail.com>
2024-08-30 21:45:20 -0300

    Added cors

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import { propiedades } from './propiedades.js';
+import cors from 'cors';
 const app = express();
 
 const port = process.env.PORT || 4000;
-
+app.use(cors());
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
